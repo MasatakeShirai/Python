a79dd44347bd3f3a05702adea747dca5863c74c7
白井優武 <masatakeshirai0814@gmail.com>
2020-04-02 00:39:17 +0900

    時間計測プログラムにコメント追加

--- chap12/TimeMeasue.py
+++ chap12/TimeMeasue.py
@@ -3,8 +3,11 @@
 def timing(f):
     import time
     def new_f(*args, **kwds):
+        #プログラム実行前のシステム時間を取る
         t = time.time()
+        #対象の実行
         result = f(*args, **kwds)
+        #関数の名前と対象の実行時間を表示
         print('%s: %ss'%(f.__name__, time.time()-t))
 
         return result
@@ -13,6 +16,5 @@ def timing(f):
 @timing
 def func():
     import time
+    #1秒待つ
     time.sleep(1)
-
-func()
