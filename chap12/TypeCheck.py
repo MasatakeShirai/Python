2a815a5fa080f3ba43db26c6342b7735d7b26550
白井優武 <masatakeshirai0814@gmail.com>
2020-04-02 01:42:29 +0900

    引数方チェックにコメント追加

--- chap12/TypeCheck.py
+++ chap12/TypeCheck.py
@@ -1,11 +1,17 @@
 #デコレータの応用例
 # 引数の型チェック
+
+#チェック基準の型をシーケンス型で受け取る
 def accepts(*types):
     def _accepts(f):
         def new_f(*args,**kwds):
+            #zipはイテラブルオブジェクトの要素をまとめる
+            # typsには引数としてstrとintが入る
+            # argsにはduplicateが呼ばれた時の引数が入る
             for arg, type_ in zip(args, types):
+                #isinstanceは第1引数にオブジェクト，第二引数に型名を取る
                 if not isinstance(arg, type_):
-                    raise TypeError('%s -> %s'%(arg, type_))
+                    raise TypeError('%s -> %s'%(arg, type_))                    
             return f(*args, **kwds)
         return new_f
     return _accepts
